9855074c5a10e8e467ceb60492a2e0ed4ce37a73
Adam Walker <adamwalker567123@gmail.com>
2023-02-16 20:52:51 -0800

    Stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Container } from "@mui/material";
 import { Header } from "./components/Header";
+import { ScheduleStepper } from "./components/ScheduleStepper";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container maxWidth={"md"}>
+        <ScheduleStepper />
+      </Container>
     </div>
   );
 }
