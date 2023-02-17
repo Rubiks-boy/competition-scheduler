3aa597aed188745225d18ef505a2f84e403653a6
Adam Walker <adamwalker567123@gmail.com>
2023-02-17 00:34:24 -0800

    Events table

---
+++ src/components/EventsView/RoundTableRow.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { IconButton, TableCell, TableRow, TextField } from "@mui/material";
+import { AddCircle, Close } from "@mui/icons-material";
+import { useSelector } from "../../app/hooks";
+import { numStationsSelector, wcifEventsSelector } from "../../app/selectors";
+import { Round } from "../../types";
+import { EVENT_NAMES } from "../../constants";
+import {
+  calcTimeForRound,
+  compPerStationsRatio,
+} from "../../utils/calculators";
+
+export const RoundTableRow = ({ round }: { round: Round }) => {
+  const { eventId, roundNum, numCompetitors, numGroups, scheduledTime } = round;
+
+  const numStations = useSelector(numStationsSelector);
+  const wcifEvents = useSelector(wcifEventsSelector);
+
+  const isFinalRound =
+    wcifEvents.find(({ id }) => id === eventId)?.rounds.length === roundNum + 1;
+
+  return (
+    <TableRow key={`${eventId}-${roundNum}`}>
+      <TableCell component="th" scope="row">
+        {EVENT_NAMES[eventId]}{" "}
+        {isFinalRound ? "final" : `Round ${roundNum + 1}`}
+      </TableCell>
+      <TableCell align="right">
+        <TextField
+          hiddenLabel
+          size="small"
+          type="number"
+          value={numCompetitors ?? ""}
+          // onChange={() => {}}
+        />
+      </TableCell>
+      <TableCell align="right">
+        <TextField
+          hiddenLabel
+          size="small"
+          type="number"
+          value={numGroups ?? ""}
+          // onChange={() => {}}
+        />
+      </TableCell>
+      <TableCell align="right">
+        {compPerStationsRatio(round, numStations)}
+      </TableCell>
+      <TableCell align="right">
+        {calcTimeForRound(eventId, numGroups)}
+      </TableCell>
+      <TableCell align="right">
+        <TextField
+          hiddenLabel
+          size="small"
+          type="number"
+          value={scheduledTime ?? ""}
+          // onChange={() => {}}
+        />
+      </TableCell>
+      <TableCell align="right">
+        <div className="events-tableIcons">
+          <IconButton
+          // onChange={() => {}}
+          >
+            <AddCircle color="primary" fontSize="medium" />
+          </IconButton>
+          <IconButton
+          // onChange={() => {}}
+          >
+            <Close color="action" fontSize="small" />
+          </IconButton>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+};
---
+++ src/components/EventsView/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { useSelector } from "../../app/hooks";
+import { roundsSelector } from "../../app/selectors";
+import { RoundTableRow } from "./RoundTableRow";
+
+import "./index.css";
+
+const EventsView = () => {
+  const rounds = useSelector(roundsSelector);
+
+  return (
+    <TableContainer component={Paper} elevation={3}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Event</TableCell>
+            <TableCell align="right">Competitors</TableCell>
+            <TableCell align="right">Groups</TableCell>
+            <TableCell align="right">Ratio</TableCell>
+            <TableCell align="right">Calc Time</TableCell>
+            <TableCell align="right">Sch Time</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rounds.map((round) => (
+            <RoundTableRow round={round} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default EventsView;
