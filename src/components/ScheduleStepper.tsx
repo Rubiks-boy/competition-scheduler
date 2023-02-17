f3e39e6d0c8ef391f2f9c66e79ead7a056950c9c
Adam Walker <adamwalker567123@gmail.com>
2023-02-16 21:04:11 -0800

    Stepper for main body

---
+++ src/components/ScheduleStepper.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const steps = ["Configure competition", "Events", "Schedule"];
+
+const StepContent = ({ activeStep }: { activeStep: number }) => {
+  switch (activeStep) {
+    case 0:
+    case 1:
+    case 2:
+      return <div>{steps[activeStep]}</div>;
+    default:
+      return <div>tbd</div>;
+  }
+};
+
+export const ScheduleStepper = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () =>
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+  const handleBack = () =>
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ width: "100%", marginBottom: "1em" }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            All steps completed - you&apos;re finished
+          </Typography>
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Box sx={{ flex: "1 1 auto" }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>{steps[activeStep]}</Typography>
+          <StepContent activeStep={activeStep} />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              pt: 2,
+            }}
+          >
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: "1 1 auto" }} />
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? "Finish" : "Next"}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
+};
