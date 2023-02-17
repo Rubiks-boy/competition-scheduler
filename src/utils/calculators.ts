c4418b03eebe18b17a907f7a585afc721b90c9ba
Adam Walker <adamwalker567123@gmail.com>
2023-02-17 00:32:42 -0800

    Helper constants + calcualtors

---
+++ src/utils/calculators.ts
@@ -0,0 +1,23 @@
+import { TIME_PER_GROUP } from "../constants";
+import { EventId, Round } from "../types";
+
+export const compPerStationsRatio = (
+  { numCompetitors, numGroups }: Round,
+  numStations: number
+) => {
+  if (!numCompetitors || !numGroups) {
+    return "";
+  }
+
+  return (numCompetitors / numGroups / numStations).toFixed(2);
+};
+
+export const calcTimeForRound = (
+  eventId: EventId,
+  numGroups: number | null
+) => {
+  const calculatedTime = TIME_PER_GROUP[eventId] * (numGroups || 0);
+
+  // round up to the nearest 5 mins
+  return Math.ceil(calculatedTime / 5) * 5;
+};
