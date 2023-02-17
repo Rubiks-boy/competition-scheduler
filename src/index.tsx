03b8c9f1c4d2c04c955b8b29290b6dfb005bd57b
Adam Walker <adamwalker567123@gmail.com>
2023-02-16 21:31:46 -0800

    initialize auth upon loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { initializeAuth } from "./utils/auth";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+initializeAuth();
 
 root.render(
   <React.StrictMode>
