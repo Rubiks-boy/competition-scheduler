37e241fd2aa670b7486e580a8a31008f2d5435c1
Adam Walker <adamwalker567123@gmail.com>
2023-02-17 20:33:23 -0800

    helper to go from rounds back to wcif events

--- src/utils/wcif.ts
+++ src/utils/wcif.ts
@@ -1,4 +1,4 @@
-import { Round, Wcif, WcifEvent } from "../types";
+import { EventId, Round, Wcif, WcifEvent, WcifRound } from "../types";
 import {
   calcExpectedNumCompetitors,
   calcNumGroups,
@@ -11,8 +11,9 @@ const wcifEventsToRounds = (
   events: Array<WcifEvent>
 ): Array<RoundWithRoundNum> => {
   return events.flatMap(({ id, rounds }) =>
-    rounds.map((_, index) => ({
+    rounds.map(({ format }, index) => ({
       eventId: id,
+      format,
       roundNum: index,
       numCompetitors: null,
       numGroups: null,
@@ -99,4 +100,68 @@ export const getDefaultRoundData = ({
   const withScheduledTimed = addScheduledTime(withNumGroups);
 
   return withScheduledTimed;
+};
+
+const getEventIds = (rounds: Array<Round>): Array<EventId> => {
+  return [...new Set(rounds.map(({ eventId }) => eventId))];
+};
+
+const getAdvanceConditionType = (
+  wcifEvents: Array<WcifEvent>,
+  eventId: string,
+  roundNum: number
+) => {
+  // const event = wcifEvents.find(({ id }) => id == eventId);
+
+  // return (
+  //   event?.rounds.find(({ id }) => id === `${eventId}-${roundNum}`)
+  //     ?.advancementCondition?.type ?? "ranking"
+  // );
+
+  // TODO: support other types
+  return "ranking" as const;
+};
+
+export const roundsToWcifEvents = (
+  rounds: Array<Round>,
+  originalWcifEvents: Array<WcifEvent>
+): Array<WcifEvent> => {
+  const eventIds = getEventIds(rounds);
+
+  return eventIds.map((id: EventId) => {
+    const roundsInEvent = rounds.filter(({ eventId }) => eventId === id);
+
+    const wcifRounds: Array<WcifRound> = roundsInEvent.map((round, index) => {
+      const numAdvancingCompetitors =
+        roundsInEvent[index + 1]?.numCompetitors ?? null;
+
+      const advancementCondition = numAdvancingCompetitors
+        ? // TODO support other types of advancement conditions
+          {
+            type: getAdvanceConditionType(originalWcifEvents, id, index + 1),
+            level: numAdvancingCompetitors,
+          }
+        : null;
+
+      const roundId = `${round.eventId}-r${index + 1}`;
+
+      return {
+        id: roundId,
+        format: round.format,
+        advancementCondition,
+        scrambleSetCount: (round.numGroups ?? 0) + 1,
+        // TODO properly set up time limits, cutoffs, other round info.
+        // grab from the previous info we have for the event?
+        // maybe for subsequent events make the time limit 10m and cutoff null?
+        timeLimit: { centiseconds: 60000, cumulativeRoundIds: [] },
+      };
+    });
+
+    return {
+      id,
+      rounds: wcifRounds,
+      qualification: null,
+      extensions: [],
+    };
+  });
 };
