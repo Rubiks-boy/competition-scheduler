3780efbfc472dcf204c2c87a42c39824274d93a5
Adam Walker <adamwalker567123@gmail.com>
2023-02-23 22:21:42 -0800

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Container } from "@mui/material";
+import React from "react";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  useMediaQuery,
+} from "@mui/material";
 import { Header } from "./components/Header";
 import { ScheduleStepper } from "./components/ScheduleStepper";
 import { useLookupAccessToken } from "./hooks/useLookupAccessToken";
@@ -6,16 +13,31 @@ import { useFetchCompetitions } from "./hooks/useFetchCompetitions";
 import { useFetchWcif } from "./hooks/useFetchWcif";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   useLookupAccessToken();
   useFetchCompetitions();
   useFetchWcif();
 
   return (
     <div className="App">
-      <Header />
-      <Container maxWidth={"md"}>
-        <ScheduleStepper />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Container maxWidth={"md"}>
+          <ScheduleStepper />
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
