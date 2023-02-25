c504526df6bb0eb5a59dcfbc9d2e643fc3084bc1
Adam Walker <adamwalker567123@gmail.com>
2023-02-25 01:33:16 -0800

    AnimatePresence

---
+++ src/components/AnimatePresence.tsx
@@ -0,0 +1,83 @@
+import classNames from "classnames";
+import React, { useEffect, useRef, useState } from "react";
+
+import "./AnimatePresence.css";
+
+const usePrevious = (val: any) => {
+  const prev = useRef(val);
+  useEffect(() => {
+    prev.current = val;
+  }, [val]);
+  return prev.current;
+};
+
+export const AnimatePresence = ({
+  children,
+  duration = 300,
+}: {
+  children: React.ReactElement;
+  duration?: number;
+}) => {
+  const previousChildren = useRef<React.ReactElement | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const previousKey = usePrevious(children?.key);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const newChild = children?.key !== previousKey;
+  const rerenderWhileAnimating = newChild && isAnimating;
+
+  useEffect(() => {
+    setIsAnimating(true);
+
+    const container = containerRef.current;
+
+    if (container) {
+      container.style.height = `${container.getBoundingClientRect()?.height}px`;
+    }
+
+    const resetAfterAnimating = () => {
+      if (container) {
+        container.style.height = "";
+      }
+      previousChildren.current = children;
+      setIsAnimating(false);
+    };
+
+    const timeout = setTimeout(() => {
+      resetAfterAnimating();
+    }, duration);
+
+    return () => {
+      resetAfterAnimating();
+      clearTimeout(timeout);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [children?.key, duration]);
+
+  return (
+    <div className="animatePresence-container" ref={containerRef}>
+      {previousChildren.current &&
+        !rerenderWhileAnimating &&
+        (newChild || isAnimating) && (
+          <div
+            className={classNames("animatePresence animatePresence--exiting", {
+              "animatePresence--exitingActive": isAnimating,
+            })}
+            key={previousChildren.current?.key}
+          >
+            {previousChildren.current}
+          </div>
+        )}
+      <div
+        className={classNames("animatePresence", {
+          "animatePresence--entering": newChild || isAnimating,
+          "animatePresence--enteringActive": isAnimating,
+        })}
+        key={children?.key}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
