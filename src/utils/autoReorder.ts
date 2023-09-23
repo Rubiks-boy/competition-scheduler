c12d520b6b707b6f1ba8c0a5df9282920c733ff0
Adam Walker <adamwalker567123@gmail.com>
2023-09-22 20:54:13 -0700

    Auto reposition dividers

--- src/utils/autoReorder.ts
+++ src/utils/autoReorder.ts
@@ -1,5 +1,6 @@
 import { IDEAL_EVENT_ORDERING } from "../constants";
 import { OtherActivity, Schedule } from "../types";
+import { range } from "./utils";
 
 const sortEventsInRound = (roundNum: number, schedule: Schedule): Schedule => {
   const eventsInRound = schedule.filter(
@@ -8,7 +9,11 @@ const sortEventsInRound = (roundNum: number, schedule: Schedule): Schedule => {
   );
 
   return IDEAL_EVENT_ORDERING.filter((eventId) =>
-    eventsInRound.find((scheduleEntry) => scheduleEntry.eventId === eventId)
+    eventsInRound.find(
+      (scheduleEntry) =>
+        scheduleEntry.type !== "day-divider" &&
+        scheduleEntry.eventId === eventId
+    )
   ).map((eventId) => ({
     type: "event",
     eventId,
@@ -26,7 +31,7 @@ export const autoReorder = (schedule: Schedule): Schedule => {
     return scheduleEntry ? [scheduleEntry] : [];
   };
 
-  return [
+  const reorderedSchedule: Schedule = [
     ...findActivity("registration"),
     ...findActivity("checkin"),
     ...findActivity("tutorial"),
@@ -35,4 +40,26 @@ export const autoReorder = (schedule: Schedule): Schedule => {
     ...[1, 2, 3].flatMap((i) => sortEventsInRound(i, schedule)),
     ...findActivity("awards"),
   ];
+
+  const numActivities = reorderedSchedule.length;
+  const numberOfDays = schedule.filter(
+    ({ type }) => type === "day-divider"
+  ).length;
+
+  // Split up the day dividers evenly among the event IDs
+  // ex. if it's a 3 day comp, 1/3 of the activities will be on day 1, 1/3 on day 3, etc.
+  // _ideally_ this would use how long the events are projected to take, but... ehhhh
+  const dividerLocations = range(numberOfDays).map(
+    (dayIndex) => Math.floor(numActivities / numberOfDays) * dayIndex
+  );
+  dividerLocations.reverse();
+  dividerLocations.forEach((dividerLoc, i) => {
+    const dayIndex = numberOfDays - (i + 1);
+    reorderedSchedule.splice(dividerLoc, 0, {
+      type: "day-divider",
+      dayIndex,
+    });
+  });
+
+  return reorderedSchedule;
 };
