507aa217ea0f62cd297a657ddc6fc9d74a55f668
Adam Walker <adamwalker567123@gmail.com>
2024-01-28 15:32:18 -0800

    comp name on every page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { SignIn } from "./SignIn";
+import {
+  activeStepSelector,
+  competitionNameSelector,
+} from "../../app/selectors";
+import { useSelector } from "../../app/hooks";
 
 export const Header = () => {
+  const competitionName = useSelector(competitionNameSelector);
+  const activeStep = useSelector(activeStepSelector);
+
   return (
     <Box sx={{ flexGrow: 1, marginBottom: "1.5em" }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Schedule generator
+          <Typography
+            variant="h1"
+            component="div"
+            sx={{ flexGrow: 1, fontSize: "1.5rem", fontWeight: "500" }}
+          >
+            {activeStep === 0
+              ? "Schedule generator"
+              : `Schedule generator – ${competitionName}`}
           </Typography>
           <SignIn />
         </Toolbar>
